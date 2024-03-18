ebf06f116a2cb5ea7dc28513ef73f6291270747b
Luis C <zs21013875@estudiantes.uv.mx>
2024-03-17 19:19:00 -0600

    first commit

---
+++ Programa2.java
@@ -0,0 +1,29 @@
+public class Programa2 {
+    private static Object hora = new Object();
+    
+    private static class JuntaThread implements Runnable {
+        public JuntaThread() {
+            hora = 9;
+        }
+
+        @Override
+        public void run() {
+            realizarJunta();
+        }
+
+        public static synchronized void realizarJunta() {
+            synchronized(hora) {
+                System.out.println(Thread.currentThread().getName() + "junta iniciada a las: " + hora);
+                hora = (int) hora + 1;
+                System.out.println(Thread.currentThread().getName() + "junta terminada a las: " + hora);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            Thread hilo1 = new Thread(new JuntaThread(), "Junta-" + (i + 1));
+            hilo1.start();
+        }
+    }
+}
